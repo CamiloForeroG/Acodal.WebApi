c6c1185072e54abd6e929d397f981e66a4bd7009
CamiloForeroG <camus35@hotmail.com>
2016-06-13 20:00:21 -0500

    Commit completo

---
+++ Acodal.WebApi/Controllers/ConferenceServiceController.cs
@@ -0,0 +1,36 @@
+﻿using Acodal.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Acodal.WebApi.Controllers
+{
+    public class ConferenceServiceController : ApiController
+    {
+        ConferenceServiceModel CsModel = new ConferenceServiceModel(); 
+
+        [HttpGet]
+        [Route("API/ConferenceService/GetConferences")]
+        public IHttpActionResult GetConferences() {
+                var model = CsModel.ManagerCB.GetConferences(null);
+                return Json(model);
+        }
+
+
+        [HttpGet]
+        [Route("API/ConferenceService/Valor")]
+        public IHttpActionResult Valor() {
+            return Ok("algo");
+        }
+
+        [HttpPost]
+        [Route("API/ConferenceService/GetConferencesByHour")]
+        public IHttpActionResult GetConferencesByHour([FromBody]string hora) {
+            var model = CsModel.ManagerCB.GetConferencesByHour(Convert.ToDateTime(hora));
+            return Json(model);
+        }
+    }
+}
